af0dfb3a91a216fafc9e3430b0d63365eba17821
AnnChar <ann.skr14@gmail.com>
2018-07-06 17:05:27 +0700

    Create MoviesDataRepository

--- app/src/main/kotlin/com/his/features/movies/data/MoviesRepository.kt
+++ app/src/main/kotlin/com/his/features/movies/data/MoviesRepository.kt
@@ -15,34 +15,11 @@
  */
 package com.his.features.movies.data
 
-import com.his.core.exception.NetworkConnectionException
-import com.his.core.platform.NetworkHandler
-import com.his.features.movies.data.entity.mapper.MovieDataMapper
 import com.his.features.movies.view.model.Movie
 import com.his.features.movies.view.model.MovieDetails
 import io.reactivex.Observable
-import javax.inject.Inject
 
 interface MoviesRepository {
 	fun movies(): Observable<List<Movie>>
 	fun movieDetails(movieId: Int): Observable<MovieDetails>
-
-	class Network
-	@Inject constructor(private val networkHandler: NetworkHandler,
-	                    private val service: MoviesService) : MoviesRepository {
-
-		override fun movies(): Observable<List<Movie>> {
-			return when (networkHandler.isConnected) {
-				true        -> service.movies().map { it.map { MovieDataMapper().toMovie(it) } }
-				false, null -> Observable.error(NetworkConnectionException())
-			}
-		}
-
-		override fun movieDetails(movieId: Int): Observable<MovieDetails> {
-			return when (networkHandler.isConnected) {
-				true        -> service.movieDetails(movieId).map { MovieDataMapper().toMovieDetails(it) }
-				false, null -> Observable.error(NetworkConnectionException())
-			}
-		}
-	}
 }
