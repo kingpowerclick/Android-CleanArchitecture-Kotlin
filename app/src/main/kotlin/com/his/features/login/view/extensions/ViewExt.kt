e6b2a8bfe30ce6ba361e7339ff74c22cb7d45e32
AnnChar <ann.skr14@gmail.com>
2018-07-31 14:45:06 +0700

    Refactor code for merge

--- app/src/main/kotlin/com/his/features/login/view/extensions/ViewExt.kt
+++ app/src/main/kotlin/com/his/features/login/view/extensions/ViewExt.kt
@@ -1,8 +1,6 @@
 package com.his.features.login.view.extensions
 
 import android.support.design.widget.TextInputLayout
-import android.view.View
-import android.view.ViewGroup
 
 fun TextInputLayout.setText(text: String?) {
 	this.editText?.setText(text)
@@ -10,24 +8,4 @@ fun TextInputLayout.setText(text: String?) {
 
 fun TextInputLayout.getText(): String? {
 	return this.editText?.text?.toString()?.trim()
-}
-
-fun TextInputLayout.disableError() {
-	this.isErrorEnabled = false
-}
-
-fun View.allChildren(): List<View> {
-	val visited = mutableListOf<View>()
-	val unvisited = mutableListOf<View>()
-	unvisited.add(this)
-	while(unvisited.isNotEmpty()) {
-		val child = unvisited.removeAt(0)
-		visited.add(child)
-		if ((child !is ViewGroup)) continue
-		(0..child.childCount).forEach {
-			unvisited.add(child.getChildAt(it))
-		}
-	}
-
-	return visited
 }
